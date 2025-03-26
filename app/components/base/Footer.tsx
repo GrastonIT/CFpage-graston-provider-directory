0f729c6b21fe4f213d38b6bff4ed2a062f594100
Xazu <Xazu001@users.noreply.github.com>
2025-03-26 10:10:22 +0100

    Update Footer.tsx

--- app/components/base/Footer.tsx
+++ app/components/base/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="pt-48 pb-8 w-full bg-white">
       <div className="container px-4 mx-auto">
         <div className="text-center">
-          <a href="" target="_blank" rel="noreferrer" className="text-xl font-bold duration-150 font-header hover:text-accent-1h"> FREE TO USE TEMPLATE</a>
+          <a href="https://github.com/Xazu001/react-router-cf" target="_blank" rel="noreferrer" className="text-xl font-bold duration-150 font-header hover:text-accent-1h"> FREE TO USE TEMPLATE</a>
         </div>
       </div>
     </footer>
