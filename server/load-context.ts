895d0613078b08606ae7ef973b411b14464afcf6
Xazu <Xazu001@users.noreply.github.com>
2025-03-26 09:53:39 +0100

    load-contet

--- server/load-context.ts
+++ server/load-context.ts
@@ -1,12 +1,13 @@
-import { Kysely } from "kysely";
-import { D1Dialect } from "kysely-d1";
 import type { AppLoadContext } from "react-router";
-import type { Tables } from "server/database/tables";
 import type { PlatformProxy } from "wrangler";
+// import type { Tables } from "server/database/tables";
+// import { Kysely } from "kysely";
+// import { D1Dialect } from "kysely-d1";
+
 
 
 interface Env {
-  db: D1Database;
+  // db: D1Database;
 }
 
 export type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
@@ -14,7 +15,7 @@ export type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 declare module "react-router" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
-    db: Kysely<Tables>;
+    // db: Kysely<Tables>;
   }
 }
 
@@ -24,14 +25,14 @@ export type GetLoadContext = (args: {
 }) => AppLoadContext;
 
 export const getLoadContext: GetLoadContext = ({ context }) => {
-  const db = new Kysely<Tables>({
-    dialect: new D1Dialect({
-      database: context.cloudflare.env.db,
-    }),
-  });
+  // const db = new Kysely<Tables>({
+  //   dialect: new D1Dialect({
+  //     database: context.cloudflare.env.db,
+  //   }),
+  // });
 
   return {
     ...context,
-    db,
+    // db,
   };
 };
